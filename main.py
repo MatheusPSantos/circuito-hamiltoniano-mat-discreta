72d2e298ad0ce8e7d4944ee9f9631df51c6a2f9c
MatheusPSantos <matheuspereira8187@gmail.com>
2017-11-30 15:29:08 -0200

    função que multiplica matrizes

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+#função que multiplica matrizes
+def multiplicar_matrizes(mat1, mat2, mat3, tam):
+    for i in range(tam):
+        for j in range(tam):
+            acumula = '0'
+            for k in range(tam):
+                acumula = acumula + mat1[i][k]*mat2[k][j]
+            mat3[i][j] = acumula
+    return mat3
+
 # Método Exato: Método "Composição Latina"
 print("Método da \"Composição Latina\"")
 # passando o tamanho das matriz a ser inserida
@@ -26,3 +37,11 @@ for i in range(qi):
         mtzB[i].append(tmp)
 print("Matriz de adjacência B : ")
 print(mtzB)
+
+# matrizes auxiliares
+mtzP1 = []
+mtzP1 = mtzA
+
+mtzP2 = []
+mtzP2 = multiplicar_matrizes(mtzB, mtzP1, mtzP2, tam)
+print(mtzP2)
